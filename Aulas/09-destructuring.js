43ddece78e486e056a4b40f814b4411737c5ef47
Lucas Aragão <lucas.magno@dcx.ufpb.br>
2020-08-29 00:02:15 -0300

    Novas resolucao

---
+++ Aulas/09-destructuring.js
@@ -0,0 +1,12 @@
+/* 
+- Implemente uma funçao que ao ser invocada, recebe um array com 3 items;
+- A funçao deve retornar um novo arrray em que:
+    - O 1 item é o 3 item do array recebido por argumento
+    - O 2 item é o 1 item do array recebido por argumento
+
+*/
+
+
+const swap = ([fisrt, , third]) => [third, fisrt]
+
+console.log(swap([953, 299, 123]));
